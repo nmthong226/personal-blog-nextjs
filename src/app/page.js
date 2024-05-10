20336e0bb99294f8f9c864958f2a1d81676248ad
nmthong226 <nmthong226@gmail.com>
2024-05-10 22:57:31 +0700

    add detail for blogsite

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,14 @@
+import FeaturePosts from "@/components/Home/FeaturePosts";
+import HomeCoverSection from "@/components/Home/HomeCoverSection";
+import RecentPosts from "@/components/Home/RecentPosts";
+import {allBlogs} from "contentlayer/generated";
 export default function Home() {
+  //console.log(allBlogs);
   return (
     <main className="flex flex-col justify-center items-center">
-      Hello world
+      <HomeCoverSection blogs={allBlogs}/>
+      <FeaturePosts blogs={allBlogs}/>
+      <RecentPosts blogs={allBlogs}/>
     </main>
   )
 }
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,23 +1,25 @@
 import React from 'react'
 import Logo from './Logo'
 import Link from 'next/link'
-
+import { Dribbble, DribbbleIcon, GithubIcon, LinkedInIcon, SunIcon, TwitterIcon } from './Icons'
 const Header = () => {
   return (
     <header className='w-full p-4 px-10 flex items-center justify-between'>
         <Logo />
         <nav className='w-max py-3 px-8 border border-solid border-dark rounded-full font-medium capitalize 
-        flex items-center space-x-4 fixed top-6 right-1/2 translate-x-1/2 bg-light/80 backdrop:blur-sm'>
+        flex items-center space-x-4 fixed top-6 right-1/2 translate-x-1/2 bg-light/80 backdrop:blur-sm z-50'>
             <Link href="/">Home</Link>
             <Link href="/about">About</Link>
             <Link href="/contact">Contact</Link>
-            <button>T</button>
+            <button>
+                <SunIcon/>
+            </button>
         </nav>
-        <div>
-            <a href='http://example.com'>LinkedIn</a>
-            <a href='http://example.com'>Twitter</a>
-            <a href='http://example.com'>Github</a>
-            <a href='http://example.com'>Dribble</a>
+        <div className='space-x-4'>
+            <a href='http://example.com' className='inline-block w-6 h-6'><LinkedInIcon  className="hover:scale-125 transition-all ease duration-200"/></a>
+            <a href='http://example.com' className='inline-block w-6 h-6'><TwitterIcon className="hover:scale-125 transition-all ease duration-200"/></a>
+            <a href='http://example.com' className='inline-block w-6 h-6'><GithubIcon className="hover:scale-125 transition-all ease duration-200"/></a>
+            <a href='http://example.com' className='inline-block w-6 h-6'><DribbbleIcon className="hover:scale-125 transition-all ease duration-200"/></a>
         </div>
     </header>
   )
