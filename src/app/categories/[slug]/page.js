facb90f75d247370a8fc7be150fc968a57007196
nmthong226 <nmthong226@gmail.com>
2024-05-12 23:03:07 +0700

    complete category page + about page

--- src/app/categories/[slug]/page.js
+++ src/app/categories/[slug]/page.js
@@ -1,31 +1,63 @@
+import BlogLayoutThree from "@/components/Blog/BlogLayoutThree";
 import Categories from "@/components/Blog/Categories";
 import { allBlogs } from "contentlayer/generated";
-import { slug } from "github-slugger";
+import GithubSlugger, { slug } from "github-slugger";
 
-const CategoryPage = ({params}) => {
+const slugger = new GithubSlugger();
+
+export async function generateStaticParams() {
+    const categories = [];
+    const paths = [{slug: "all"}];
+    
+    allBlogs.map(blog => {
+        if (blog.isPublished) {
+            blog.tags.map(tag => {
+                let slugified = slugger.slug(tag);
+                if (!categories.includes(slugified)){
+                    categories.push(slugified);
+                    paths.push({slug: slugified})
+                } 
+            })
+        }
+    })
+
+    return paths;
+}
+
+const CategoryPage = ({ params }) => {
     const allCategories = ["all"];
-    const blog = allBlogs.filter((blog) => {
+    const blogs = allBlogs.filter((blog) => {
         return blog.tags.some((tag) => {
             const slugified = slug(tag);
-            if (!allCategories.includes(slugified)){
+            if (!allCategories.includes(slugified)) {
                 allCategories.push(slugified);
             }
-            if (params.slug === "all"){
+            if (params.slug === "all") {
                 return true;
             }
             return slugified === params.slug
         })
     })
-    console.log("!@#", allCategories)
     return (
-        <article>
-            <div>
-                <h1>
+        <article className="mt-12 flex flex-col text-dark">
+            <div className="px-32 flex flex-col">
+                <h1 className="mt-6 font-semibold text-5xl">
                     #{params.slug}
                 </h1>
-                <span>Discover more categories and expand your knowledge!</span>
+                <span className="mt-2 inline-block">Discover more categories and expand your knowledge!</span>
             </div>
-            <Categories categories={allCategories} currentSlug={params.slug}/>
+            <Categories categories={allCategories} currentSlug={params.slug} />
+            <div className="grid grid-cols-3 grid-rows-2 gap-16 mt-24 px-32">
+                {
+                    blogs.map((blog, index) => {
+                        return (
+                            <article key={index} className="col-span-1 row-span-1 relative">
+                                <BlogLayoutThree blog={blog} />
+                            </article>
+                        )
+                    })
+                }
+            </div>
         </article>
     )
 }
