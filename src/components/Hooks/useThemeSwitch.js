bbb627ad298b0658ba5618f0852ab8cd3d624667
nmthong226 <nmthong226@gmail.com>
2024-05-15 19:07:41 +0700

    [fix-bug] theme mode

--- src/components/Hooks/useThemeSwitch.js
+++ src/components/Hooks/useThemeSwitch.js
@@ -1,9 +1,10 @@
-"use client"
-import React, { useEffect, useState } from 'react'
+'use client'
+import { useState, useEffect } from 'react';
 
-export default function useThemeSwitch(){
-    const preferDarkQuery = "(prefers-color-schema:dark)";
+export default function useThemeSwitch() {
+    const preferDarkQuery = "(prefers-color-scheme: dark)";
     const storageKey = "theme";
+
     const toggleTheme = (theme) => {
         if (theme === "dark") {
             document.documentElement.classList.add("dark");
@@ -11,31 +12,42 @@ export default function useThemeSwitch(){
             document.documentElement.classList.remove("dark");
         }
         window.localStorage.setItem(storageKey, theme);
-    }
+    };
+
     const getUserPreference = () => {
-        const userPref = window.localStorage.getItem(storageKey);
-        if (userPref) {
-            return userPref;
+        if (typeof window !== 'undefined') {
+            const userPref = window.localStorage.getItem(storageKey);
+            if (userPref) {
+                return userPref;
+            }
+            return window.matchMedia(preferDarkQuery).matches ? "dark" : "light";
         }
-        return window.matchMedia(preferDarkQuery).matches ? "dark" : "light"
-    }
-    const [mode, setMode] = useState("dark");
+        return "light"; // Default to light if window is not defined
+    };
+
+    const [mode, setMode] = useState("light");
+    const [isMounted, setIsMounted] = useState(false);
+
     useEffect(() => {
+        setIsMounted(true);
         const mediaQuery = window.matchMedia(preferDarkQuery);
         const handleChange = () => {
             const newMode = getUserPreference();
             setMode(newMode);
             toggleTheme(newMode);
-        }
+        };
         handleChange();
         mediaQuery.addEventListener("change", handleChange);
         return () => {
             mediaQuery.removeEventListener("change", handleChange);
-        }
+        };
     }, []);
 
     useEffect(() => {
-        toggleTheme(mode)
-    }, [mode]);
+        if (isMounted) {
+            toggleTheme(mode);
+        }
+    }, [mode, isMounted]);
+
     return [mode, setMode];
 }
