194fc7569a92f4067106efc039477017fae56d1a
nmthong226 <nmthong226@gmail.com>
2024-05-16 23:26:09 +0700

    mini changes

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,6 @@ import HomeCoverSection from "@/components/Home/HomeCoverSection";
 import RecentPosts from "@/components/Home/RecentPosts";
 import {allBlogs} from "contentlayer/generated";
 export default function Home() {
-  //console.log(allBlogs);
   return (
     <main className="flex flex-col justify-center items-center">
       <HomeCoverSection blogs={allBlogs}/>
