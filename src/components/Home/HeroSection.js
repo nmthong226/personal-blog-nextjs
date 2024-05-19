72f2f504f494146acf66c0409cf868a1684b8806
nmthong226 <nmthong226@gmail.com>
2024-05-19 13:37:58 +0700

    add details

--- src/components/Home/HeroSection.js
+++ src/components/Home/HeroSection.js
@@ -7,10 +7,10 @@ import { IconDownload, IconLink } from '@tabler/icons-react';
 
 const HeroSection = () => {
     return (
-        <section className='h-auto lg:h-[85vh] grid grid-cols-1 lg:grid-cols-12 items-center justify-center text-dark dark:text-light border rounded-2xl border-dark dark:border-light mx-5 sm:mx-10 bg-beige dark:bg-dark'>
-            <div className='col-span-12 lg:col-span-7 px-10 sxl:px-0 sxl:pl-10 space-y-8 my-10 lg:my-0'>
+        <section className='group h-auto lg:h-[85vh] grid grid-cols-1 lg:grid-cols-12 items-center justify-center text-dark dark:text-light border rounded-2xl border-dark dark:border-light mx-5 sm:mx-10 bg-beige dark:bg-dark'>
+            <div className='order-2 lg:order-1 col-span-12 lg:col-span-7 px-10 sxl:px-0 sxl:pl-10 space-y-2 xs:space-y-8 mb-2 xs:mb-10 lg:mb-0'>
                 <h1 className='flex flex-col xs:flex-row text-dark dark:text-light text-2xl sm:text-3xl md:text-4xl sxl:text-5xl font-extrabold'>
-                    <span className='text-transparent bg-clip-text bg-gradient-to-r from-darkBlue to-lightBlue dark:from-purpleBlue dark:to-purplePink mr-2'>Hello, I'm {" "}</span>
+                    <span className='text-transparent bg-clip-text bg-gradient-to-r from-darkBlue to-lightBlue dark:from-[#FF2718] dark:to-[#FFF801] mr-2'>Hello, I'm {" "}</span>
                     <TypeAnimation
                         sequence={[
                             'Thong Nguyen',
@@ -28,10 +28,11 @@ const HeroSection = () => {
                     />
                 </h1>
                 <p className='text-dark dark:text-light text-base md:text-lg lg:text-xl'>
-                    I kinda love sharing things, so this is my be-side place to share my own memories and stories. Welcome to my living page!
+                    I kinda love sharing things, so this is my be-side place to 
+                    share my own memories and stories. Welcome to my living page!
                 </p>
                 <div className='flex xs:flex-row flex-col'>
-                    <button className='flex bg-gradient-to-r from-darkBlue to-lightBlue dark:from-purpleBlue dark:to-purplePink  p-4 rounded-lg text-center text-light xs:mb-0 mb-2 xs:mr-2 mr-0'>
+                    <button className='flex bg-gradient-to-r from-darkBlue to-lightBlue dark:from-[#FF2718]/90 dark:to-[#FFF801]/90  p-4 rounded-lg text-center text-light xs:mb-0 mb-2 xs:mr-2 mr-0'>
                         <IconLink className='mr-2' />
                         My portfolio
                     </button>
@@ -41,11 +42,12 @@ const HeroSection = () => {
                     </button>
                 </div>
             </div>
-            <div className='col-span-12 lg:col-span-5 p-10 flex justify-center'>
+            <div className='order-1 lg:order-2 col-span-12 lg:col-span-5 p-2 xs:p-10 flex justify-center'>
                 <Image
                     src={avatar}
-                    className='w-full  rounded-3xl'
                     alt='my-avatar'
+                    className='w-full h-full object-center object-cover rounded-2xl'
+                    priority
                 />
             </div>
         </section>
