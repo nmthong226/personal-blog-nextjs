9483dbcc8a4cf9bf6e962608e3f674a47cf4c012
nmthong226 <nmthong226@gmail.com>
2024-10-02 22:44:42 +0700

    style: add responsive

--- src/app/about-me/page.js
+++ src/app/about-me/page.js
@@ -32,18 +32,23 @@ const spotifyApi = new SpotifyWebApi();
 const AboutMe = () => {
     return (
         <div className='flex flex-col lg:flex-row w-full h-full justify-center lg:justify-end bg-white overflow-y-auto'>
-            <div className='flex flex-col h-full lg:w-[30%] sxl:w-1/3 lg:fixed lg:left-0 p-10 sxl:p-20'>
-                <div className='w-48 h-48 border-[1px] rounded-full items-center justify-center flex hover:cursor-pointer'>
+            <div className='flex flex-col h-full lg:w-[30%] sxl:w-1/3 lg:fixed lg:left-0 p-10 sxl:p-20 max-lg:mt-64'>
+                <div className='max-lg:hidden w-48 h-48 border-[1px] rounded-full items-center justify-center flex hover:cursor-pointer'>
                     <Image src={avatar} alt='avatar' className='w-44 h-44 rounded-full' />
                 </div>
-                <div className='mt-6'>
-                    <h1 className='text-3xl sxl:text-4xl font-extrabold'>Thong Nguyen</h1>
-                    <ul className='space-y-3 mt-6 text-lg sxl:text-xl text-zinc-700 whitespace-nowrap'>
-                        <li>💼 Web Developer</li>
-                        <li>💻 UI/UX Desiger</li>
-                        <li>🌏 Stayed in HCMC, VietNam</li>
-                        <li>🎂 Since 22/06/2003</li>
-                    </ul>
+                <div className='flex flex-col xs:flex-row mt-32 xs:mt-6 space-x-0 xs:space-x-6'>
+                    <div className='lg:hidden w-36 h-36 md:w-48 md:h-48 border-[1px] rounded-full items-center justify-center flex hover:cursor-pointer'>
+                        <Image src={avatar} alt='avatar' className='w-32 h-32 md:w-44 md:h-44 rounded-full' />
+                    </div>
+                    <div>
+                        <h1 className='text-lg xs:text-xl md:text-3xl sxl:text-4xl font-extrabold max-xs:mt-4'>Thong Nguyen</h1>
+                        <ul className='space-y-3 mt-6 text-sm xs:text-base md:text-lg sxl:text-xl text-zinc-700 whitespace-nowrap'>
+                            <li>💼 Web Developer</li>
+                            <li>💻 UI/UX Desiger</li>
+                            <li>🌏 Stayed in HCMC, VietNam</li>
+                            <li>🎂 Since 22/06/2003</li>
+                        </ul>
+                    </div>
                 </div>
             </div>
             <div className='flex flex-col p-10 sxl:p-20 w-full lg:w-[70%] sxl:w-[60%] h-full gap-6'>
@@ -55,7 +60,7 @@ const AboutMe = () => {
                     </div>
                     <div className='col-span-1 row-span-1 bg-zinc-900 text-white rounded-3xl h-[164px] order-2'>
                         <div className='flex flex-col w-full h-full p-2 justify-center items-center'>
-                            <p className='text-xl font-bold text-center whitespace-pre-line flex-shrink-0'>
+                            <p className='text-sm xs:text-xl font-bold text-center whitespace-pre-line flex-shrink-0'>
                                 Obsession{'\n'}
                                 beats talent{'\n'}
                                 every time.
@@ -170,19 +175,19 @@ const AboutMe = () => {
                         </div>
                         <div className='flex px-2 justify-center items-center absolute z-10 bg-gray-50 rounded-md text-zinc-600 bottom-0 text-sm m-2 border-2'>Curry</div>
                     </div>
-                    <div className='col-span-1 row-span-1 bg-gradient-to-r from-[#833AB4] via-[#FD1D1D] to-[#FCB045] hover:opacity-95 hover:cursor-pointer text-white rounded-3xl h-[164px] relative order-2'>
+                    <div className='col-span-1 row-span-1 bg-gradient-to-r from-[#833AB4] via-[#FD1D1D] to-[#FCB045] hover:opacity-95 hover:cursor-pointer text-white rounded-3xl h-[164px] relative order-3 md:order-2'>
                         <div className='flex flex-col w-full h-full justify-center border-2 rounded-2xl items-center'>
                             <Image src={img_fried} alt='img-arknights' className='w-full h-full object-cover rounded-2xl' />
                         </div>
                         <div className='flex px-2 justify-center items-center absolute z-10 bg-gray-50 rounded-md text-zinc-600 bottom-0 text-sm m-2 border-2'>Fried shrimp</div>
                     </div>
-                    <div className='col-span-1 row-span-2 bg-gradient-to-r from-[#833AB4] via-[#FD1D1D] to-[#FCB045] hover:opacity-95 hover:cursor-pointer text-white rounded-3xl h-[352px] relative order-6'>
+                    <div className='col-span-1 row-span-2 bg-gradient-to-r from-[#833AB4] via-[#FD1D1D] to-[#FCB045] hover:opacity-95 hover:cursor-pointer text-white rounded-3xl h-[352px] relative order-4 md:order-3'>
                         <div className='flex flex-col w-full h-full justify-center border-2 rounded-2xl items-center'>
                             <Image src={gif_pho} alt='img-arknights' className='w-full h-full object-cover rounded-2xl' />
                         </div>
                         <div className='flex px-2 justify-center items-center absolute z-10 bg-gray-50 rounded-md text-zinc-600 bottom-0 text-sm m-2 border-2'>Noodles</div>
                     </div>
-                    <div className='col-span-2 row-span-1 bg-gradient-to-b from-[#3b5998] to-[#052d66] hover:opacity-95 hover:cursor-pointer text-white rounded-3xl h-[164px] relative order-4'>
+                    <div className='col-span-2 row-span-1 bg-gradient-to-b from-[#3b5998] to-[#052d66] hover:opacity-95 hover:cursor-pointer text-white rounded-3xl h-[164px] relative order-2 md:order-4'>
                         <div className='flex flex-col w-full h-full justify-center border-2 rounded-2xl items-center'>
                             <Image src={gif_egg} alt='img-arknights' className='w-full h-full object-cover rounded-2xl' />
                         </div>
