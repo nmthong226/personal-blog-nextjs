d9526faf58892f144a34012b897f205325e6fd5a
nmthong226 <nmthong226@gmail.com>
2024-10-03 23:25:26 +0700

    feat(gallery): add more content

--- src/app/about-me/page.js
+++ src/app/about-me/page.js
@@ -14,6 +14,7 @@ import img_reverse1999 from '@/public/img-reverse1999.png';
 import img_carrie from '@/public/img-carrie.png';
 import img_fried from '@/public/img-fried.png';
 import img_thai from '@/public/img-thai.png';
+import img_cancer from '@/public/img-cancer.png';
 import gif_stardew from '@/public/gif-stardewvalley.gif';
 import gif_cat from '@/public/gif-cat.gif';
 import gif_pho from '@/public/gif-pho.gif';
@@ -32,17 +33,17 @@ const spotifyApi = new SpotifyWebApi();
 const AboutMe = () => {
     return (
         <div className='flex flex-col lg:flex-row w-full h-full justify-center lg:justify-end bg-white overflow-y-auto'>
-            <div className='flex flex-col h-full lg:w-[30%] sxl:w-1/3 lg:fixed lg:left-0 p-10 sxl:p-20 max-lg:mt-64'>
+            <div className='flex flex-col h-full lg:w-[30%] sxl:w-1/3 lg:fixed lg:left-0 p-3 xs:p-10 sxl:p-20 max-lg:mt-64'>
                 <div className='max-lg:hidden w-48 h-48 border-[1px] rounded-full items-center justify-center flex hover:cursor-pointer'>
                     <Image src={avatar} alt='avatar' className='w-44 h-44 rounded-full' />
                 </div>
                 <div className='flex flex-col xs:flex-row mt-32 xs:mt-6 space-x-0 xs:space-x-6'>
-                    <div className='lg:hidden w-36 h-36 md:w-48 md:h-48 border-[1px] rounded-full items-center justify-center flex hover:cursor-pointer'>
-                        <Image src={avatar} alt='avatar' className='w-32 h-32 md:w-44 md:h-44 rounded-full' />
+                    <div className='lg:hidden w-36 h-36 sm:w-48 sm:h-48 border-[1px] rounded-full items-center justify-center flex hover:cursor-pointer'>
+                        <Image src={avatar} alt='avatar' className='w-32 h-32 sm:w-44 sm:h-44 rounded-full' />
                     </div>
                     <div>
-                        <h1 className='text-lg xs:text-xl md:text-3xl sxl:text-4xl font-extrabold max-xs:mt-4'>Thong Nguyen</h1>
-                        <ul className='space-y-3 mt-6 text-sm xs:text-base md:text-lg sxl:text-xl text-zinc-700 whitespace-nowrap'>
+                        <h1 className='text-lg xs:text-xl sm:text-3xl sxl:text-4xl font-extrabold max-xs:mt-4'>Thong Nguyen</h1>
+                        <ul className='space-y-3 mt-6 text-sm xs:text-base sm:text-lg sxl:text-xl text-zinc-700 whitespace-nowrap'>
                             <li>💼 Web Developer</li>
                             <li>💻 UI/UX Desiger</li>
                             <li>🌏 Stayed in HCMC, VietNam</li>
@@ -51,7 +52,7 @@ const AboutMe = () => {
                     </div>
                 </div>
             </div>
-            <div className='flex flex-col p-10 sxl:p-20 w-full lg:w-[70%] sxl:w-[60%] h-full gap-6'>
+            <div className='flex flex-col p-3 xs:p-10 sxl:p-20 w-full lg:w-[70%] sxl:w-[60%] h-full gap-6'>
                 <h1 className='font-bold text-lg'>About me 🙋‍♂️</h1>
                 <div className='grid grid-cols-2 grid-rows-4 md:grid-cols-4 md:grid-rows-2 h-[724px] md:h-[352px] gap-6'>
                     <div className='flex col-span-1 row-span-2  text-white rounded-3xl relative h-[352px] order-1'>
@@ -77,20 +78,11 @@ const AboutMe = () => {
                             <Image src={gif_cat} alt='img-cat' className='w-full h-full object-cover rounded-2xl' />
                         </div>
                     </div>
-                    <div className='flex col-span-1 row-span-1 bg-white hover:bg-gray-50 hover:cursor-pointer border-2 text-zinc-900 rounded-3xl relative h-[164px] order-3 md:order-5'>
-                        <a href='https://nmthong226.vercel.app/'
-                            target='_blank'
-                            className='flex flex-col w-full h-full p-2 justify-center '>
-                            <h1 className='text-base font-bold line-clamp-1'>
-                                My Portfolio
-                            </h1>
-                            <span className='text-sm text-slate-500 line-clamp-1'>
-                                nmthong226.vercel.app
-                            </span>
-                        </a>
-                        <div className='flex w-10 h-10 border-2 rounded-xl absolute items-center justify-center m-2'>
-                            <FaRegFileLines className='w-6 h-6 rotate-[20deg] top-0' />
+                    <div className='col-span-1 row-span-1 bg-zinc-400 text-white rounded-3xl h-[164px] order-5 relative'>
+                        <div className='flex flex-col w-full h-full justify-center items-center'>
+                            <Image src={img_cancer} alt='img-cancer' className='w-full h-full object-cover rounded-2xl' />
                         </div>
+                        <div className='flex px-2 justify-center items-center absolute z-10 bg-gray-50 rounded-md text-zinc-600 bottom-0 text-sm m-2 border-2'>♋ Cancer</div>
                     </div>
                     <div className='flex col-span-2 row-span-1 md:col-span-2 md:row-span-1 rounded-3xl relative order-6'>
                         <div className='flex flex-col w-full h-full justify-center items-center'>
@@ -106,15 +98,13 @@ const AboutMe = () => {
                             <Image src={img_code} alt='img-code' className='w-full h-full object-cover rounded-2xl' />
                         </div>
                     </div>
-                    <div className='col-span-1 row-span-1 bg-[#0a2748] text-white rounded-3xl h-[164px]'>
+                    <div className='col-span-1 row-span-1 bg-gray-100 text-zinc-700 border-2 rounded-3xl h-[164px] relative'>
                         <div className='flex flex-col w-full h-full p-2 justify-center items-center'>
                             <p className='text-xl font-bold whitespace-pre-line flex-shrink-0'>
-                                speaks 👄{'\n'}
-                                VN{'\n'}
-                                EN{'\n'}
-                                JP.
+                                VN, EN, JP
                             </p>
                         </div>
+                        <div className='flex px-2 justify-center items-center absolute z-10 bg-gray-50 rounded-md text-zinc-600 bottom-0 text-sm m-2 border-2'>👄 Speak</div>
                     </div>
                     <div className='flex col-span-2 row-span-2 rounded-3xl relative'>
                         <iframe src="https://open.spotify.com/embed/playlist/3zptUL73sNzy6weGlOQ1RP?utm_source=generator&theme=0" width="100%" height="352" frameBorder="0" allowFullScreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
@@ -201,7 +191,7 @@ const AboutMe = () => {
                     </div>
                 </div>
                 <h1 className='font-bold text-lg mt-16'>My socials 📫</h1>
-                <div className='grid grid-cols-2 grid-rows-3 md:grid-cols-4 md:grid-rows-2 gap-6 h-[550px] md:h-[352px]'>
+                <div className='grid grid-cols-2 grid-rows-3 md:grid-cols-4 md:grid-rows-1 gap-6 h-[550px] md:h-[352px]'>
                     <div className='col-span-1 row-span-1 bg-gradient-to-b from-[#535252] to-[#0c0c0c] hover:opacity-95 hover:cursor-pointer text-white rounded-3xl h-[164px] relative'>
                         <div className='flex flex-col justify-between w-full h-full p-4'>
                             <BsGithub className='text-white w-10 h-10' />
@@ -248,6 +238,24 @@ const AboutMe = () => {
                         </div>
                         <div className='absolute right-0 top-0 m-4 p-1 rounded-full bg-white/60'>
                             <FaArrowUp className='rotate-45 w-4 h-4' />
+                        </div>
+                    </div>
+                </div>
+                <h1 className='font-bold text-lg mt-16'>My others ✨</h1>
+                <div className='grid grid-cols-2 grid-rows-3 md:grid-cols-4 md:grid-rows-2 gap-6 h-[550px] md:h-[352px]'>
+                    <div className='flex col-span-1 row-span-1 bg-white hover:bg-gray-50 hover:cursor-pointer border-2 text-zinc-900 rounded-3xl relative h-[164px] order-3 md:order-5'>
+                        <a href='https://nmthong226.vercel.app/'
+                            target='_blank'
+                            className='flex flex-col w-full h-full p-2 justify-center '>
+                            <h1 className='text-base font-bold line-clamp-1'>
+                                My Portfolio
+                            </h1>
+                            <span className='text-sm text-slate-500 line-clamp-1'>
+                                nmthong226.vercel.app
+                            </span>
+                        </a>
+                        <div className='flex w-10 h-10 border-2 rounded-xl absolute items-center justify-center m-2'>
+                            <FaRegFileLines className='w-6 h-6 rotate-[20deg] top-0' />
                         </div>
                     </div>
                 </div>
